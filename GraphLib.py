df4eb35749d3d502c1e76facfc188744f4c0b07c
JuanPineda115 <pin19087@uvg.edu.gt>
2021-11-28 15:00:06 -0600

    almost done

--- GraphLib.py
+++ GraphLib.py
@@ -14,8 +14,8 @@ class Model(object):
         self.position = glm.vec3(0, 0, 0)
         self.rotation = glm.vec3(0, 0, 0)
         self.scale = glm.vec3(1, 1, 1)
-        self.textureSurface = image.load(textureName)
-        self.textureData = image.tostring(self.textureSurface, "RGB", True)
+        self.textSurf = image.load(textureName)
+        self.textureData = image.tostring(self.textSurf, "RGB", True)
         self.texture = glGenTextures(1)
 
     def getModelMatrix(self):
@@ -37,100 +37,102 @@ class Model(object):
             for i in range(3):
                 # positions
                 pos = self.model.vertices[face[i][0] - 1]
-                self.appendToBuffer(buffer, pos)
+                buffer.append(pos[0])
+                buffer.append(pos[1])
+                buffer.append(pos[2])
                 # normals
                 norm = self.model.normals[face[i][2] - 1]
-                self.appendToBuffer(buffer, norm)
+                buffer.append(norm[0])
+                buffer.append(norm[1])
+                buffer.append(norm[2])
                 # texCoords
                 uvs = self.model.texcoords[face[i][1] - 1]
                 buffer.append(uvs[0])
                 buffer.append(uvs[1])
         self.vertBuffer = array(buffer, dtype=float32)
-        self.VBO = glGenBuffers(1)  # Vertex Buffer Object
-        self.VAO = glGenVertexArrays(1)  # Vertex Array Object
-
-    def appendToBuffer(self, buffer, arg1):
-        buffer.append(arg1[0])
-        buffer.append(arg1[1])
-        buffer.append(arg1[2])
-
-    def clearBuffer(self):
-        self.vertBuffer = []
+        self.VBO = glGenBuffers(1)  
+        self.VAO = glGenVertexArrays(1)  
 
     def renderInScene(self):
         glBindVertexArray(self.VAO)
         glBindBuffer(GL_ARRAY_BUFFER, self.VBO)
         # Los vertices
-        glBufferData(
-            GL_ARRAY_BUFFER,
-            self.vertBuffer.nbytes,
-            self.vertBuffer,
-            GL_STATIC_DRAW)
+        glBufferData(GL_ARRAY_BUFFER,  
+                    self.vertBuffer.nbytes,
+                    self.vertBuffer,  
+                    GL_STATIC_DRAW)
         # Atributo de posicion
-        glVertexAttribPointer(
-            0, 3, GL_FLOAT, GL_FALSE,
-            4 * 8, ctypes.c_void_p(0))
+        glVertexAttribPointer(0,               
+                                3,               
+                                GL_FLOAT,        
+                                GL_FALSE,         
+                                4 * 8,           
+                                ctypes.c_void_p(0)) 
         glEnableVertexAttribArray(0)
         # Atributo de normales
-        glVertexAttribPointer(
-            1, 3, GL_FLOAT, GL_FALSE,
-            4 * 8,  ctypes.c_void_p(4 * 3))
+        glVertexAttribPointer(1,
+                                3,
+                                GL_FLOAT,
+                                GL_FALSE,
+                                4 * 8,
+                                ctypes.c_void_p(4 * 3))
         glEnableVertexAttribArray(1)
         # Atributo de coordenadas de textura
-        glVertexAttribPointer(
-            2, 2, GL_FLOAT, GL_FALSE,
-            4 * 8, ctypes.c_void_p(4 * 6))
+        glVertexAttribPointer(2,
+                                2,
+                                GL_FLOAT,
+                                GL_FALSE,
+                                4 * 8,
+                                ctypes.c_void_p(4 * 6))
         glEnableVertexAttribArray(2)
         # Dar textura
         glBindTexture(GL_TEXTURE_2D, self.texture)
-        glTexImage2D(
-            GL_TEXTURE_2D, 0, GL_RGB, self.textureSurface.get_width(),
-            self.textureSurface.get_height(), 0, GL_RGB, GL_UNSIGNED_BYTE,
-            self.textureData)
+        glTexImage2D(GL_TEXTURE_2D,                     
+                    0,                                 
+                    GL_RGB,                            
+                    self.textSurf.get_width(),   
+                    self.textSurf.get_height(),  
+                    0,                                 
+                    GL_RGB,                            
+                    GL_UNSIGNED_BYTE,                  
+                    self.textureData)                  
         glGenerateMipmap(GL_TEXTURE_2D)
         # Dibujar
         glDrawArrays(GL_TRIANGLES, 0, len(self.model.faces)
-                     * 3)  # Para dibujar vertices en orden
+                     * 3)  
 
 
 class Renderer(object):
-    def __init__(self, screen, toRender):
+    def __init__(self, screen):
         self.screen = screen
         _, _, self.width, self.height = screen.get_rect()
         glEnable(GL_DEPTH_TEST)
         glViewport(0, 0, self.width, self.height)
         self.scene = []
         self.pointLight = glm.vec3(-10, 0, -5)
-        self.tiempo = 0
-        self.valor = 0
+        self.dTime = 0
+        self.normals = 0
         # View Matrix
         self.camPosition = glm.vec3(0, 0, 0)
-        self.camRotation = glm.vec3(0, 0, 0)  # pitch, yaw, roll
+        self.camRotation = glm.vec3(0, 0, 0)  
         # Projection Matrix
-        self.projectionMatrix = glm.perspective(
-            glm.radians(60), self.width / self.height,
-            0.1, 1000)
-        self.activeModel = toRender
-        self.viewMatrix = self.getViewMatrix()
-
-    def lookAt(self, eye, camPosition):
-        front = camPosition - eye
-        # print('despues de la resta', camPosition, eye, front)
-        front = front / glm.normalize(front)
-        right = glm.cross(glm.vec3(0, 1, 0), front)
-        right = right / glm.normalize(right)
-        up = glm.cross(front, right)
-        up = up / glm.normalize(up)
-        self.camMatrix = glm.mat4(
-            [right.x, right.y, right.z, 0],
-            [up.x, up.y, up.z, 0],
-            [front.x, front.y, front.z, 0],
-            [camPosition.x, camPosition.y, camPosition.z, 1])
-        self.viewMatrix = glm.inverse(self.camMatrix)
+        self.projectionMatrix = glm.perspective(glm.radians(60),            
+                                                self.width / self.height,
+                                                0.1,                        
+                                                1000)                       
 
     def getViewMatrix(self):
-        self.camMatrix = self.activeModel.getModelMatrix()
-        return glm.inverse(self.camMatrix)
+        identity = glm.mat4(1)
+        translateMatrix = glm.translate(identity, self.camPosition)
+        pitch = glm.rotate(identity, glm.radians(
+            self.camRotation.x), glm.vec3(1, 0, 0))
+        yaw = glm.rotate(identity, glm.radians(
+            self.camRotation.y), glm.vec3(0, 1, 0))
+        roll = glm.rotate(identity, glm.radians(
+            self.camRotation.z), glm.vec3(0, 0, 1))
+        rotationMatrix = pitch * yaw * roll
+        camMatrix = translateMatrix * rotationMatrix
+        return glm.inverse(camMatrix)
 
     def wireframeMode(self):
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE)
@@ -138,10 +140,6 @@ class Renderer(object):
     def filledMode(self):
         glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
 
-# can recieve up to 3 shaders
-# each shader as a list with
-#    name as first value
-#    name in shader.py as second value
     def setShaders(self, vertexShader, fragShader):
         if vertexShader is not None and fragShader is not None:
             self.active_shader = compileProgram(compileShader(vertexShader, GL_VERTEX_SHADER),
@@ -149,30 +147,23 @@ class Renderer(object):
         else:
             self.active_shader = None
 
-    def update(self):
-        print('en el pupdat', self.activeModel.position)
-        self.viewMatrix = self.lookAt(glm.vec3(0,0,0), self.camPosition)
-
     def render(self):
         glClearColor(0.2, 0.2, 0.2, 1)
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         glUseProgram(self.active_shader)
         if self.active_shader:
-            glUniformMatrix4fv(
-                glGetUniformLocation(self.active_shader, "viewMatrix"),
-                1, GL_FALSE, glm.value_ptr(self.getViewMatrix()))
-            glUniformMatrix4fv(
-                glGetUniformLocation(self.active_shader, "projectionMatrix"),
-                1, GL_FALSE, glm.value_ptr(self.projectionMatrix))
+            glUniformMatrix4fv(glGetUniformLocation(self.active_shader, "viewMatrix"),
+                                1, GL_FALSE, glm.value_ptr(self.getViewMatrix()))
+            glUniformMatrix4fv(glGetUniformLocation(self.active_shader, "projectionMatrix"),
+                                1, GL_FALSE, glm.value_ptr(self.projectionMatrix))
             glUniform1f(glGetUniformLocation(
-                self.active_shader, "tiempo"), self.tiempo)
+                self.active_shader, "dTime"), self.dTime)
             glUniform1f(glGetUniformLocation(
-                self.active_shader, "valor"), self.valor)
+                self.active_shader, "normals"), self.normals)
             glUniform3f(glGetUniformLocation(self.active_shader, "pointLight"),
                         self.pointLight.x, self.pointLight.y, self.pointLight.z)
         for model in self.scene:
             if self.active_shader:
-                glUniformMatrix4fv(
-                    glGetUniformLocation(self.active_shader, "modelMatrix"),
-                    1, GL_FALSE, glm.value_ptr(model.getModelMatrix()))
+                glUniformMatrix4fv(glGetUniformLocation(self.active_shader, "modelMatrix"),
+                                    1, GL_FALSE, glm.value_ptr(model.getModelMatrix()))
             model.renderInScene()
--- Main.py
+++ Main.py
@@ -1,71 +1,79 @@
+from OpenGL.raw.GL.VERSION.GL_1_0 import GL_GREEN
 import pygame
 from pygame.locals import *
-import numpy as np
+import glm
 from GraphLib import Renderer, Model
 import shaders
-import glm
 
-
-width = 960
-height = 540
-
+width = 1600
+height = 900
 deltaTime = 0.0
-
+pygame.mixer.init()
 pygame.init()
 screen = pygame.display.set_mode((width,height), pygame.DOUBLEBUF | pygame.OPENGL )
 clock = pygame.time.Clock()
 
-
-thang = Model('src/Objects/Pedang.obj', 'src/Textures/brighter.png')
-rend = Renderer(screen, thang)
+rend = Renderer(screen)
 rend.setShaders(shaders.vertex_shader, shaders.fragment_shader)
-thang.position.z = -5
-rend.scene.append( thang )
+uniqueModel = Model('src/Objects/katana.obj', 'src/Textures/brighter.png')
+uniqueModel.position.z = -7
+uniqueModel.position.x = -5
+uniqueModel.rotation.x = -90
+# uniqueModel.position.y = -6
+uniqueModel.scale = glm.vec3(0.2, 0.2, 0.2)
+rend.scene.append( uniqueModel )
+pygame.mixer.music.load('src/sounds/Retro.mp3')
+#otherwise its too loud
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
 
 isRunning = True
 while isRunning:
+    pygame.mixer.music.fadeout(99999)
+    pygame.display.set_caption("Python OpenGL by Juan Pablo Pineda")
     keys = pygame.key.get_pressed()
+    mouse = pygame.mouse.get_pressed()
+    mouse2 = pygame.MOUSEBUTTONDOWN;
     # Traslacion de camara
     if keys[K_d]:
-        rend.camPosition.x += 3 * deltaTime
+        rend.camPosition.x += 1 * deltaTime
     if keys[K_a]:
-        rend.camPosition.x -= 3 * deltaTime
-    if keys[K_w]:
-        rend.camPosition.z -= 3 * deltaTime
+        rend.camPosition.x -= 1 * deltaTime
     if keys[K_s]:
-        rend.camPosition.z += 3 * deltaTime
-    if keys[K_q]:
-        rend.camPosition.y -= 3 * deltaTime
-    if keys[K_e]:
-        rend.camPosition.y += 3 * deltaTime
-
-    if keys[K_LEFT] and rend.valor > 0:
-        rend.valor -= 0.1 * deltaTime
-
-    if keys[K_RIGHT] and rend.valor < 0.2:
-        rend.valor += 0.1 * deltaTime
+        rend.camPosition.y -= 1 * deltaTime
+    if keys[K_w]:
+        rend.camPosition.y += 1 * deltaTime
+    if keys[K_UP]:
+        if rend.normals > 0:
+            rend.normals -= 0.1 * deltaTime
+    if keys[K_DOWN]:
+        if rend.normals < 0.2:
+            rend.normals += 0.1 * deltaTime
 
     # Rotacion de camara
-    if keys[K_z]:
+    if keys[K_q]:
         rend.camRotation.y += 15 * deltaTime
-    if keys[K_x]:
+        rend.camPosition.x += 1.5 * deltaTime
+    if keys[K_e]:
         rend.camRotation.y -= 15 * deltaTime
-
-
+        rend.camPosition.x -= 1.5 * deltaTime
     for ev in pygame.event.get():
         if ev.type == pygame.QUIT:
             isRunning = False
-
         elif ev.type == pygame.KEYDOWN:
             if ev.key == pygame.K_ESCAPE:
                 isRunning = False
-
             if ev.key == K_1:
                 rend.filledMode()
             if ev.key == K_2:
                 rend.wireframeMode()
+        elif ev.type == pygame.MOUSEBUTTONDOWN:
+            if ev.button == 4:
+                rend.camPosition.z -= 3 * deltaTime
+            if ev.button == 5:
+                rend.camPosition.z += 3 * deltaTime
 
-    rend.tiempo += deltaTime
+    rend.dTime += deltaTime
     deltaTime = clock.tick(60) / 1000
 
     rend.render()
